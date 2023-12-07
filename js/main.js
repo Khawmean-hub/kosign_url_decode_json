8ed496d6b94558e129cd8319a4564d7bf9e67b9d
Khaw Mean <khawmean8@gmail.com>
2023-12-07 09:49:39 +0700

    new feature HTML 2 JS

--- js/main.js
+++ js/main.js
@@ -255,6 +255,75 @@ function onCheckExcludeSave() {
     }
 }
 
+
+
+function validateVariable(variablename) {
+    const re = /[a-zA-Z_][a-zA-Z0-9_]*/gm;
+    return re.test(variablename);
+}
+
+function updateEmailValidity(variable) {
+    const email = variable.value;
+    const emailIsValid = validateVariable(email);
+
+    if (emailIsValid) {
+        $(variable).parent().removeClass('error')
+    } else {
+        $(variable).parent().addClass('error')
+    }
+}
+
+function html2jsResult(){
+    const html = $('#raw_html').val();
+
+    const qout = $("#is_double_q").prop("checked") ? '"' : "'";
+    let vari = $('#vari_nm').val() || 'result';
+    vari = vari.trim();
+    const showVari = $("#is_show_vari").prop("checked") ? $("#type_vari").text()+' ' + vari + " = '';\n" : '';
+    const regex = /^\s+|\n/g;
+    // const newText = html.replace(regex, "'$&'");
+    const nArray = html.split(regex);
+    nArray.forEach((v, i)=>{
+        if(v.trim() !== ''){
+            if($("#is_remove_whitespace").prop("checked")) {
+                nArray[i] =vari + " += " +  v.replace(/^( *)([^\s])/gm, qout+"$2") + qout;
+            }else{
+                nArray[i] =vari + " += " +  v.replace(/^( *)([^\s])/gm, "$1"+qout+"$2") + qout;
+            }
+        }
+    })
+
+    if($("#is_remove_line").prop("checked")){
+        //remove empty line
+        nArray.forEach((v, i)=>{
+            if(v.trim() === ''){
+                nArray.splice(i, 1);
+            }
+        })
+    }
+
+    const newText = showVari + nArray.join("\n");
+
+    $("#result_html").val(newText);
+
+}
+
+$(document).on('input', '#vari_nm', function () {
+    updateEmailValidity(this);
+});
+
+
+// HTML 2 JS
+$(document).on('change, input', '#vari_nm, #raw_html, #setting_box input', function () {
+    html2jsResult();
+})
+
+$(document).on('click', '#vari_type', function () {
+    html2jsResult();
+})
+
+
+
 onCheckExcludeSave();
 buildCheck();
 // checkLiveMode()
