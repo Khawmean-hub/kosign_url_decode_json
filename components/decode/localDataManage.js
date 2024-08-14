a84807aef75ae4bd993ad934ca9a4d126637a564
Khaw <74886008+Khawmean-hub@users.noreply.github.com>
2024-08-14 13:15:34 +0900

    fixed4

--- components/decode/localDataManage.js
+++ components/decode/localDataManage.js
@@ -106,7 +106,11 @@ function buildJsonMenuList() {
     $('.btn_box_close').dropdown();
 
     if(getThemeName().includes('dark')){
-        $('#save_data_rec [style="background: #f5f5f5"]').attr('style', 'background: #151515')
+        if(getThemeName().includes('dark3')){
+            $('#save_data_rec .box').attr('style', 'background-image: radial-gradient( circle farthest-corner at 3.2% 49.6%,  rgba(80,12,139,0.87) 0%, rgba(161,10,144,0.72) 83.6% )')
+        }else {
+            $('#save_data_rec [style="background: #f5f5f5"]').attr('style', 'background: #151515')
+        }
     }
 }
 
--- components/nav_bar/nav_bar.js
+++ components/nav_bar/nav_bar.js
@@ -12,7 +12,12 @@ function onChangeThemeColor() {
     if(themeText.includes('dark')){
         $('body').removeAttr('class').addClass(themeText)
         $('.ui.input,.ui.checkbox, .ui.modal, .ui.form, .ui.table').addClass('inverted')
-        $('#save_data_rec [style="background: #f5f5f5"]').attr('style', 'background: #151515')
+        if(themeText.includes('dark3')){
+            $('#save_data_rec .box').attr('style', 'background-image: radial-gradient( circle farthest-corner at 3.2% 49.6%,  rgba(80,12,139,0.87) 0%, rgba(161,10,144,0.72) 83.6% )')
+        }else {
+            $('#save_data_rec [style="background: #f5f5f5"]').attr('style', 'background: #151515')
+        }
+        
         editroDark()
         setThemeName(themeText);
     }else{
